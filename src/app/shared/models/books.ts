21a86f158319162772db1d0aacfc8f3a2a6a50c4
ianthomas0 <ianthomas0@live.com>
2023-03-31 11:33:16 -0400

    Search by book index instead of book name (#8)

--- src/app/shared/models/books.ts
+++ src/app/shared/models/books.ts
@@ -13,172 +13,241 @@ export class Books {
     { name: '1 Samuel' },
     { name: '2 Samuel' },
     {
-      name: '1 Kings'
+      name: '1 Kings',
     },
     {
-      name: '2 Kings'
+      name: '2 Kings',
     },
     {
-      name: '1 Chronicles'
+      name: '1 Chronicles',
     },
     {
-      name: '2 Chronicles'
+      name: '2 Chronicles',
     },
     {
-      name: 'Ezra'
+      name: 'Ezra',
     },
     {
-      name: 'Nehemiah'
+      name: 'Nehemiah',
     },
     {
-      name: 'Esther'
+      name: 'Esther',
     },
     {
-      name: 'Job'
+      name: 'Job',
     },
     {
-      name: 'Psalms'
+      name: 'Psalms',
     },
     {
-      name: 'Proverbs'
+      name: 'Proverbs',
     },
     {
-      name: 'Ecclesiastes'
+      name: 'Ecclesiastes',
     },
     {
-      name: 'Song of Solomon'
+      name: 'Song of Solomon',
     },
     {
-      name: 'Isaiah'
+      name: 'Isaiah',
     },
     {
-      name: 'Jeremiah'
+      name: 'Jeremiah',
     },
     {
-      name: 'Lamentations'
+      name: 'Lamentations',
     },
     {
-      name: 'Ezekiel'
+      name: 'Ezekiel',
     },
     {
-      name: 'Daniel'
+      name: 'Daniel',
     },
     {
-      name: 'Hosea'
+      name: 'Hosea',
     },
     {
-      name: 'Joel'
+      name: 'Joel',
     },
     {
-      name: 'Amos'
+      name: 'Amos',
     },
     {
-      name: 'Obadiah'
+      name: 'Obadiah',
     },
     {
-      name: 'Jonah'
+      name: 'Jonah',
     },
     {
-      name: 'Micah'
+      name: 'Micah',
     },
     {
-      name: 'Nahum'
+      name: 'Nahum',
     },
     {
-      name: 'Habakkuk'
+      name: 'Habakkuk',
     },
     {
-      name: 'Zephaniah'
+      name: 'Zephaniah',
     },
     {
-      name: 'Haggai'
+      name: 'Haggai',
     },
     {
-      name: 'Zechariah'
+      name: 'Zechariah',
     },
     {
-      name: 'Malachi'
+      name: 'Malachi',
     },
     {
-      name: 'Matthew'
+      name: 'Matthew',
     },
     {
-      name: 'Mark'
+      name: 'Mark',
     },
     {
-      name: 'Luke'
+      name: 'Luke',
     },
     {
-      name: 'John'
+      name: 'John',
     },
     {
-      name: 'Acts'
+      name: 'Acts',
     },
     {
-      name: 'Romans'
+      name: 'Romans',
     },
     {
-      name: '1 Corinthians'
+      name: '1 Corinthians',
     },
     {
-      name: '2 Corinthians'
+      name: '2 Corinthians',
     },
     {
-      name: 'Galatians'
+      name: 'Galatians',
     },
     {
-      name: 'Ephesians'
+      name: 'Ephesians',
     },
     {
-      name: 'Philippians'
+      name: 'Philippians',
     },
     {
-      name: 'Colossians'
+      name: 'Colossians',
     },
     {
-      name: '1 Thessalonians'
+      name: '1 Thessalonians',
     },
     {
-      name: '2 Thessalonians'
+      name: '2 Thessalonians',
     },
     {
-      name: '1 Timothy'
+      name: '1 Timothy',
     },
     {
-      name: '2 Timothy'
+      name: '2 Timothy',
     },
     {
-      name: 'Titus'
+      name: 'Titus',
     },
     {
-      name: 'Philemon'
+      name: 'Philemon',
     },
     {
-      name: 'Hebrews'
+      name: 'Hebrews',
     },
     {
-      name: 'James'
+      name: 'James',
     },
     {
-      name: '1 Peter'
+      name: '1 Peter',
     },
     {
-      name: '2 Peter'
+      name: '2 Peter',
     },
     {
-      name: '1 John'
+      name: '1 John',
     },
     {
-      name: '2 John'
+      name: '2 John',
     },
     {
-      name: '3 John'
+      name: '3 John',
     },
     {
-      name: 'Jude'
+      name: 'Jude',
     },
     {
-      name: 'Revelation'
-    }
+      name: 'Revelation',
+    },
   ];
+
+  public static bookOrderMap: any = {
+    Genesis: 1,
+    Exodus: 2,
+    Leviticus: 3,
+    Numbers: 4,
+    Deuteronomy: 5,
+    Joshua: 6,
+    Judges: 7,
+    Ruth: 8,
+    '1 Samuel': 9,
+    '2 Samuel': 10,
+    '1 Kings': 11,
+    '2 Kings': 12,
+    '1 Chronicles': 13,
+    '2 Chronicles': 14,
+    Ezra: 15,
+    Nehemiah: 16,
+    Esther: 17,
+    Job: 18,
+    Psalms: 19,
+    Proverbs: 20,
+    Ecclesiastes: 21,
+    'Song of Solomon': 22,
+    Isaiah: 23,
+    Jeremiah: 24,
+    Lamentations: 25,
+    Ezekiel: 26,
+    Daniel: 27,
+    Hosea: 28,
+    Joel: 29,
+    Amos: 30,
+    Obadiah: 31,
+    Jonah: 32,
+    Micah: 33,
+    Nahum: 34,
+    Habakkuk: 35,
+    Zephaniah: 36,
+    Haggai: 37,
+    Zechariah: 38,
+    Malachi: 39,
+    Matthew: 40,
+    Mark: 41,
+    Luke: 42,
+    John: 43,
+    Acts: 44,
+    Romans: 45,
+    '1 Corinthians': 46,
+    '2 Corinthians': 47,
+    Galatians: 48,
+    Ephesians: 49,
+    Philippians: 50,
+    Colossians: 51,
+    '1 Thessalonians': 52,
+    '2 Thessalonians': 53,
+    '1 Timothy': 54,
+    '2 Timothy': 55,
+    Titus: 56,
+    Philemon: 57,
+    Hebrews: 58,
+    James: 59,
+    '1 Peter': 60,
+    '2 Peter': 61,
+    '1 John': 62,
+    '2 John': 63,
+    '3 John': 64,
+    Jude: 65,
+    Revelation: 66,
+  };
 }
